5f7a7424ae1af58d02203d3bbab5374e2f463567
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-08-22 11:51:31 +0900

    練習問題5-1修正

--- Main.java
+++ Main.java
@@ -5,12 +5,16 @@ public class Main {
     String name = "Tomoya Kobayashi";
     int old = 54;
     float height = 176;
-    String eto = "羊";
+    char eto = '羊';
     System.out.println("私の名前は " + name + " です");
     System.out.println("年齢は " + old + " で、身長は " + height + " です");
     System.out.println("干支は " + eto + " になります");
     
   }
+
+  // charと、Javaで文字列を表すクラスのStringは、お互いに違ったモノ同士です。
+  // charは一文字を表す数字なのに対して、JavaのStringは0文字以上の文字が集まったモノで、
+  // Stringクラスのインスタンスです。
 
   public static void main(String[] args) { //argとはargumentの略で、引数という意味
     introduceOneself();
