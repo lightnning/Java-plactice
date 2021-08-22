0dbbdfd7bbaafd6baefe69604d8ccc136ac7e3da
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-08-22 13:12:12 +0900

    練習5-4解答

--- Main.java
+++ Main.java
@@ -1,30 +1,18 @@
 public class Main {
   
 
-  public static void email(String title, String address, String text){
-    title = "こんにちわ";
-    address = "aaa@gmal.com";
-    text = "メールを送ります";
+  public static void calcTriangleArea(double bottom, double height){
 
-    System.out.println(address + " に、以下のメールを送信しました");
-    System.out.println("件名： " + title);
-    System.out.println("本文: " + text);    
+    double triangleArea = bottom * height / 2;
+    return triangleArea;
+      
   }
 
-  public static void email(String address, String text){
-    address = "aaa@gmal.com";
-    text = "メールを送ります";
-
-    System.out.println(address + " に、以下のメールを送信しました");
-    System.out.println("件名: 無題");
-    System.out.println("本文: " + text);    
-  }
+  
 
 
   public static void main(String[] args) { //argとはargumentの略で、引数という意味
-    String address = "aaa@gmal.com";
-    String text = "メールを送ります";
 
-    email(address, text);
+    System.out.println("");
   }
   }
