0d378aeaee8dd1f8bbbc6506c2e193d27dcee715
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-08-22 11:46:44 +0900

    練習問題5-1解答

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
 public class Main {
   
 
-  public static void incArray(int[] array){
-    for (int i = 0; i < array.length; i++){
-      array[i]++;
-    }
+  public static void introduceOneself(){
+    String name = "Tomoya Kobayashi";
+    int old = 54;
+    float height = 176;
+    String eto = "羊";
+    System.out.println("私の名前は " + name + " です");
+    System.out.println("年齢は " + old + " で、身長は " + height + " です");
+    System.out.println("干支は " + eto + " になります");
+    
   }
 
   public static void main(String[] args) { //argとはargumentの略で、引数という意味
-    int[] array = {1, 2, 3};
-    incArray(array);
-    for (int i : array) {
-      System.out.println(i);
-    }
+    introduceOneself();
   }
   }
