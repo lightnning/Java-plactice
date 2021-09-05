482a23962e7960e66929d43e38056ae0c0dc67ed
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-09-05 22:12:56 +0900

    練習問題1

---
+++ plactice_08_test/Cleric.java
@@ -0,0 +1,12 @@
+public class Cleric {
+  String name;
+  int hp = 50;
+  final int MAX_HP = 50;
+  int mp = 10;
+  final int MAX_MP = 10;
+  
+  public void selfAid() {
+    this.mp -= 5;
+    this.hp = 50;
+  }
+}
