40a6c574cf5d6716d38e9238f62a2614c579b5ab
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-09-05 16:25:31 +0900

    お化けキノコclass作成

--- plactice_08/Main.java
+++ plactice_08/Main.java
@@ -5,13 +5,19 @@ public class Main{
     // 2.フィールドに初期値をセット
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました！");
-    // 3.勇者のメソッドを呼び出していく
-    h.sleep();
-    h.sit(5);
+
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
+
+    Matango m2 = new Matango();
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    // 冒険の始まり
     h.slip();
-    h.sit(25);
-    h.sit(10);
+    m1.run();
+    m2.run();
     h.run();
         
   }
