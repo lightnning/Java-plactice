e4b0276674c93fbdada1ee32934de67a47a4484d
Tomoya_Kobayashi <spde6g59@arrow.ocn.ne.jp>
2022-05-21 08:10:17 +0900

    フォルダ整理

--- 2021/plactice_08/Main.java
+++ 2021/plactice_08/Main.java
@@ -6,6 +6,8 @@ public class Main{
     h.name = "ミナト";
     h.hp = 100;
 
+    System.out.println("勇者" + h.name + "を生み出した！");
+
     Matango m1 = new Matango();
     m1.hp = 50;
     m1.suffix = 'A';
@@ -15,6 +17,7 @@ public class Main{
     m2.suffix = 'B';
 
     // 冒険の始まり
+    h.sit(2);
     h.slip();
     m1.run();
     m2.run();
